4b078ae22717105e93190dab47239a5c2728dbef
Karol Lopez <knle21597@gmail.com>
2018-06-24 22:42:34 -0500

    Cambios
    Cambio en el menú. (DOCTORES MEDICAMENTOS)
    Cambio aside.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,27 @@
-(function(){
-	function onSuccess(data){
-		console.log(data);
-	}
+function onSuccess(data){
+	//$("#quotes").append("<div></div>");
+	$(data).find('cita').each(function(){
+	    var autor = $(this).find('autor').text();
+		var texto_c = $(this).find('texto').text();
+		$("<div></div>").html(autor + ", " + texto_c).appendTo("#quotes ");
+    });
+
+}
+function cargarCitas(){
 	
+
 	$.ajax({
-       type: "GET",
-       url: "data/doctores.xml",
-       contentType: "text/xml",
-       dataType: "xml",
-       success: onSuccess,
-	})
+		type: "GET",
+		url: "data/citas.xml",
+		contentType: "text/xml",
+		dataType: "xml",
+		success: onSuccess
+	});
+}
+(function(){
+	
+	document.getElementById("gye").onclick = cargarCitas;
+
 })();
 
-function buscador(){	
 
-var datos=document.getElementById('nombre');
-var clases=document.querySelectorAll("div[class='well']");
-for(elemento of clases){
-	if(elemento.textContent.search(datos)>0 && datos!=''){
-		console.log(elemento);
-    }
-  }
-}
