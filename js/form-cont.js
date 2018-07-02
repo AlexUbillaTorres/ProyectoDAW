e792c60b0fb620057a703d38de52d1cfebb038de
Karol Lopez <knle21597@gmail.com>
2018-07-01 23:46:24 -0500

    ....
    ...........

--- js/form-cont.js
+++ js/form-cont.js
@@ -2,7 +2,13 @@ function env(){
     alert('Formulario Enviado');
 }
 
+function com(){
+    alert('Comentario añadido');
+}
+
 (function(){
     document.getElementById("btnenviar").onclick = env;
+    document.getElementById("btn1").onclick = com;
+    document.getElementById("btn2").onclick = com;
     
 })();
